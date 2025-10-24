b8ac277ed2e8e3790dde85bc78eec8c1d06a54bc
liptwo <142609541+liptwo@users.noreply.github.com>
2025-10-24 12:33:48 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
+// ...existing code...
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
-import Home from './pages/home'
 import { Login } from './pages/Login'
 import { Checkout } from './pages/Checkout'
 import Layout from './layouts'
+import Home from './pages/Home'
 
 function App() {
   return (
     <Routes>
-<!--       <Route path='/' element={<Home />} /> -->
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='checkout' element={<Checkout />} />
+        {/* thêm các route con ở đây */}
+      </Route>
+
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Login />} />
       <Route path='/cart' />
-      <Route path='/checkout' element={<Checkout />} />
-      <!-- <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} />
-         <Route path='hold' element={<Hold />} /> 
-      </Route> -->
     </Routes>
   )
 }
